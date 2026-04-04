4d52aa1e1a8614e3f0b9e87864a2835bf2abc25d
SteliosKrom <stelios.yolokrom@gmail.com>
2026-04-04 11:45:41 +0300

    feat: initial scriptable objects setup

---
+++ Assets/Game - Stelios/Scripts/GameData/PowerUpData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewPowerUp", menuName = "Game Data/PowerUp")]
+public class PowerUpData : GameData
+{
+    [SerializeField] private enum PowerUpType { Health, Speed}
+    [SerializeField] private PowerUpType type;
+    [SerializeField] private float duration;
+    [SerializeField] private Sprite[] animationFrames;
+}
